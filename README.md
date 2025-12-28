4b658f6c584b8379644f1277fcf59b0e45c4cd75
someretical <29365738+someretical@users.noreply.github.com>
2025-12-28 16:08:00 +1100

    first commit in ages

--- README.md
+++ README.md
@@ -4,11 +4,9 @@ A simple 2D physics sandbox game where you can place blocks of different materia
 
 ![demo](./img/demo.png)
 
-
 ## Downloading
 
 Head to the [actions tab](https://github.com/someretical/pixel-physics/actions) and access the latest build. Download the artifact that matches your operating system. Linux users will be able to choose between GNU and clang builds. Extract the contents of the archive and run the executable.
-
 
 ## Controls
 
@@ -17,12 +15,12 @@ The brush is the highlighted square under the cursor.
 - Left click (M1) to set all pixels in the brush area to the selected material
 - Right click (M2) to erase all pixels in the brush area
 - Scroll up and down to increase and decrease the size of the brush respectively
-	- Hold left control to change the brush size in increments of 10
+  - Hold left control to change the brush size in increments of 10
 - Middle click to set the brush material to the material of the pixel directly under the cursor
 - Press F11 to toggle borderless fullscreen
 
-
 ### Available materials
+
 - Press 1 to select regular sand
 - Press 2 to select red sand (more dense than regular sand)
 - Press 3 to select water
@@ -30,30 +28,74 @@ The brush is the highlighted square under the cursor.
 
 More features to come...
 
-
 ## Building
+
+Download sources
 
 ```
 git clone --recurse-submodules https://github.com/someretical/pixel-physics.git
-cd pixel-physics
 ```
 
-Choose any of the following
-```
-cmake -DCMAKE_BUILD_TYPE=Debug -S . -B cmake-build-debug-[your compiler]
-cmake --build cmake-build-debug-[your compiler] --config=Debug --target pixels -j [no. of threads]
+Add submodule
 
-cmake -DCMAKE_BUILD_TYPE=Release -S . -B cmake-build-release-[your compiler]
-cmake --build cmake-build-release-[your compiler] --config=Release --target pixels -j [no. of threads]
+```
+git submodule add -b <branch-name> <repository-url> <folder>
 ```
 
-`RelWithDebInfo` and `MinSizeRel` build types are also available but I'm too lazy to write them here.
+Update submodules to latest commit
 
-Binaries will be in `cmake-build-debug-[your compiler]` and `cmake-build-release-[your compiler]` respectively.
-
-
-### Updating submodules
 ```
-git submodule foreach --recursive git checkout master
-git submodule update --remote --merge
+git submodule update --init --recursive --remote
+```
+
+Checkout branch for specific submodule
+
+```
+git config -f .gitmodules submodule.<path_to_submodule>.branch <branch_name>
+git submodule update --remote --recursive
+```
+
+E.g, `git config -f .gitmodules submodule.dependencies/SDL.branch main`
+
+Checkout tag for specific submodule
+
+```
+cd dependencies/<submodule>
+git fetch --tags
+git checkout tags/<tag_name>
+```
+
+Don't forget to commit the submodule updates! Also use `git submodule status` to see the paths and status.
+
+## Windows
+
+1. Install MSVC with Clang support
+2. Install the VSCode clangd extension
+   - The extension won't automatically find the clangd installed under Visual Studio so the path will need to be provided. E.g, `E:\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\x64\bin\clangd.exe`
+3. Install the VSCode CMake extension and use the following settings
+
+```json
+"cmake.generator": "Ninja Multi-Config",
+"cmake.preferredGenerators": ["Ninja Multi-Config"]
+```
+
+If the generator is set to Microsoft Visual Studio 2022 (which uses MSBuild under the hood), no `compile_commands.json` will be generated and clangd won't work properly.
+
+4. Select the CMake icon from the activity bar on the left side and under the Configure dropdown choose the right kit (e.g, MSVC, clang, g++) and build (e.g, Debug, Release)
+5. The Build (different from the build mentioned above), Test, Debug, and Launch dropdowns apply to the current configuration
+
+Alternatively, set everything up from the CLI
+
+```
+cmake -DCMAKE_C_COMPILER=<FILL_IN> -DCMAKE_CXX_COMPILER=<FILL_IN> --no-warn-unused-cli -S <pixel-physics-path> -B <build-folder> -G "Ninja Multi-Config"
+
+cmake --build e:/pixel-physics/build --config Debug
+```
+
+E.g, (copy pasted from VSCode CMake extension output)
+
+```
+"E:\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE "-DCMAKE_C_COMPILER:FILEPATH=E:\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\x64\bin\clang-cl.exe" "-DCMAKE_CXX_COMPILER:FILEPATH=E:\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\x64\bin\clang-cl.exe" --no-warn-unused-cli -S E:/pixel-physics -B e:/pixel-physics/build -G "Ninja Multi-Config"
+
+"E:\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" --build e:/pixel-physics/build --config Debug
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,136 +1,202 @@
-#define SDL_MAIN_USE_CALLBACKS
-
-#include "AppContext.h"
-#include "gui.h"
-#include "sim.h"
-#include "simulator.h"
-#include "util.h"
-
-// Can't remove this include
+#define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-#include <SDL3/SDL_events.h>
-#include <SDL3/SDL_init.h>
-#include <SDL3/SDL_keycode.h>
-#include <SDL3/SDL_log.h>
-#include <SDL3/SDL_mouse.h>
-#include <SDL3/SDL_render.h>
-#include <SDL3/SDL_stdinc.h>
-#include <SDL3/SDL_surface.h>
-#include <SDL3/SDL_timer.h>
-#include <SDL3/SDL_video.h>
-#include <glm/ext/vector_float2.hpp>
+#include <backends/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_sdl3.h>
+#include <glad/gl.h>
+#include <imgui.h>
 
-#include <utility>
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <SDL3/SDL_opengles2.h>
+#else
+#include <SDL3/SDL_opengl.h>
+#endif
 
-volatile bool physics_thread_stop_token = false;
+#ifdef __EMSCRIPTEN__
+#include "../libs/emscripten/emscripten_mainloop_stub.h"
+#endif
 
-SDL_AppResult SDL_AppInit(void **appstate, [[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
-    auto res{ AppContext::Create() };
-    if (not res.has_value()) {
-        return SDL_Fail();
-    }
-    *appstate = res.value().release();
+#include <print>
 
-    SDL_Log("Application started successfully!");
+static SDL_Window *window = NULL;
+static SDL_GLContext gl_context = NULL;
 
-    return SDL_APP_CONTINUE;
-}
+constexpr static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
-    auto *app = (AppContext *)appstate;
-    SDL_ConvertEventToRenderCoordinates(app->renderer, event);
-
-    switch (event->type) {
-        case SDL_EVENT_MOUSE_WHEEL: {
-            const auto mod{ SDL_GetModState() };
-            const int d_radius{ (mod & SDL_KMOD_LCTRL) ? 5 : 1 };
-            if (event->wheel.y > 0) {
-                app->cursor.brush_radius = std::min(app->cursor.brush_radius + d_radius, gui::max_radius);
-            } else if (event->wheel.y < 0) {
-                app->cursor.brush_radius = std::max(app->cursor.brush_radius - d_radius, gui::min_radius);
-            }
-            break;
-        }
-        case SDL_EVENT_MOUSE_BUTTON_DOWN: {
-            switch (event->button.button) {
-                case SDL_BUTTON_MIDDLE: {
-                    if (check_in_lvl_range({ static_cast<int>(event->button.x), static_cast<int>(event->button.y) })) {
-                        const auto &buf{ app->chunk.buffers.getLatestFrame() };
-                        app->cursor.selected_material =
-                            buf[static_cast<int>(event->button.y)][static_cast<int>(event->button.x)].material;
-                    }
-                    break;
-                }
-            }
-
-            break;
-        }
-        case SDL_EVENT_KEY_DOWN: {
-            switch (event->key.key) {
-                case SDLK_1: {
-                    app->cursor.selected_material = sim::mat_t::Sand;
-                    SDL_Log("Selected material: Sand");
-                    break;
-                }
-                case SDLK_2: {
-                    app->cursor.selected_material = sim::mat_t::RedSand;
-                    SDL_Log("Selected material: Red Sand");
-                    break;
-                }
-                case SDLK_3: {
-                    app->cursor.selected_material = sim::mat_t::Water;
-                    SDL_Log("Selected material: Water");
-                    break;
-                }
-                case SDLK_4: {
-                    app->cursor.selected_material = sim::mat_t::Oil;
-                    SDL_Log("Selected material: Oil");
-                    break;
-                }
-                case SDLK_F11: {
-                    SDL_SetWindowFullscreen(app->window, !(SDL_GetWindowFlags(app->window) & SDL_WINDOW_FULLSCREEN));
-                    break;
-                }
-                case SDLK_ESCAPE: {
-                    app->app_quit = SDL_APP_SUCCESS;
-                    break;
-                }
-                default: {
-                    break;
-                }
-            }
-            break;
-        }
-        case SDL_EVENT_QUIT: {
-            app->app_quit = SDL_APP_SUCCESS;
-            break;
-        }
-        default: {
-            break;
-        }
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
+    {
+        std::println("Error: SDL_Init(): {}", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
-    return SDL_APP_CONTINUE;
-}
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100 (WebGL 1.0)
+    const char *glsl_version = "#version 100";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+#elif defined(IMGUI_IMPL_OPENGL_ES3)
+    // GL ES 3.0 + GLSL 300 es (WebGL 2.0)
+    const char *glsl_version = "#version 300 es";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+#elif defined(__APPLE__)
+    // GL 3.2 Core + GLSL 150
+    const char *glsl_version = "#version 150";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+#else
+    // GL 3.0 + GLSL 130
+    const char *glsl_version = "#version 130";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+#endif
 
-SDL_AppResult SDL_AppIterate(void *appstate) {
-    auto begin{ SDL_GetTicks() };
-    auto *app{ (AppContext *)appstate };
-
-    process_rendering(app);
-
-    auto elapsed_ticks = SDL_GetTicks() - begin;
-    if (elapsed_ticks < 16) {
-        SDL_Delay(static_cast<uint32_t>(16 - elapsed_ticks));
+    // Create window with graphics context
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+    SDL_WindowFlags window_flags =
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN | SDL_WINDOW_HIGH_PIXEL_DENSITY;
+    window = SDL_CreateWindow("Dear ImGui SDL3+OpenGL3 example", (int)(1280 * main_scale), (int)(800 * main_scale),
+                              window_flags);
+    if (window == nullptr)
+    {
+        printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    gl_context = SDL_GL_CreateContext(window);
+    if (gl_context == nullptr)
+    {
+        printf("Error: SDL_GL_CreateContext(): %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
-    return app->app_quit;
+    if (!gladLoadGL(SDL_GL_GetProcAddress))
+    {
+        printf("Failed to initialize OpenGL loader!\n");
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
+    SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    SDL_ShowWindow(window);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    // ImGui::StyleColorsLight();
+
+    // Setup scaling
+    ImGuiStyle &style = ImGui::GetStyle();
+    style.ScaleAllSizes(main_scale); // Bake a fixed style scale. (until we have a solution for dynamic style scaling,
+                                     // changing this requires resetting Style + calling this again)
+    style.FontScaleDpi = main_scale; // Set initial font scale. (using io.ConfigDpiScaleFonts=true makes this
+                                     // unnecessary. We leave both here for documentation purpose)
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL3_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Load Fonts
+    // - If fonts are not explicitly loaded, Dear ImGui will call AddFontDefault() to select an embedded font: either
+    // AddFontDefaultVector() or AddFontDefaultBitmap().
+    //   This selection is based on (style.FontSizeBase * style.FontScaleMain * style.FontScaleDpi) reaching a small
+    //   threshold.
+    // - You can load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - If a file cannot be loaded, AddFont functions will return a nullptr. Please handle those errors in your code
+    // (e.g. use an assertion, display an error and quit).
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use FreeType for higher quality font rendering.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double
+    // backslash \\ !
+    // - Our Emscripten build process allows embedding fonts to be accessible at runtime from the "fonts/" folder. See
+    // Makefile.emscripten for details.
+    // style.FontSizeBase = 20.0f;
+    // io.Fonts->AddFontDefaultVector();
+    // io.Fonts->AddFontDefaultBitmap();
+    // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf");
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf");
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf");
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf");
+    // ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf");
+    // IM_ASSERT(font != nullptr);
+
+    return SDL_APP_CONTINUE; /* carry on with the program! */
 }
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-    delete (AppContext *)appstate;
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    ImGui_ImplSDL3_ProcessEvent(event);
+    if (event->type == SDL_EVENT_QUIT)
+        return SDL_APP_SUCCESS;
+    if (event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event->window.windowID == SDL_GetWindowID(window))
+        return SDL_APP_SUCCESS;
 
+    return SDL_APP_CONTINUE; /* carry on with the program! */
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED)
+    {
+        SDL_Delay(10);
+        return SDL_APP_CONTINUE;
+    }
+
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
+
+    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code
+    // to learn more about Dear ImGui!).
+    bool p_open = true;
+    ImGui::ShowDemoWindow(&p_open);
+
+    // Rendering
+    ImGui::Render();
+    ImGuiIO &io = ImGui::GetIO();
+    glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+    glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w,
+                 clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    SDL_GL_SwapWindow(window);
+
+#ifdef __EMSCRIPTEN__
+    EMSCRIPTEN_MAINLOOP_END;
+#endif
+
+    return SDL_APP_CONTINUE; /* carry on with the program! */
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DestroyContext(gl_context);
+    SDL_DestroyWindow(window);
     SDL_Quit();
-    SDL_Log("Application quit %s!", result == SDL_APP_SUCCESS ? "successfully" : "with error");
 }
